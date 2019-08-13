5b7c37e47596d76989306bc5603303e15518fc57
Manny Sangha <freelance2@inkagency.com>
2019-08-13 04:14:17 +0000

    Created a view for index of contacts.

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row justify-content-center">
+	 <div class="col-md-8">
+			<div class="card">
+				<h1 class="card-header">Contacts</h1>
+			<div class="card-body">
+				<table class="table table-striped">
+					<thead>
+							<tr>
+								<td>ID</td>
+								<td>Name</td>
+								<td>Street</td>
+								<td>City</td>
+								<td>State</td>
+								<td>Zip</td>
+								<td>Mobile Phone</td>
+								<td>Home Phone</td>
+								<td colspan = 2>Actions</td>
+							</tr>
+					</thead>
+					<tbody>
+							@foreach($contacts as $contact)
+							<tr>
+									<td>{{$contact->id}}</td>
+									<td>{{$contact->First_Name}} {{$contact->Last_Name}}</td>
+									<td>{{$contact->Street}}</td>
+									<td>{{$contact->City}}</td>
+									<td>{{$contact->State}}</td>
+									<td>{{$contact->Zip}}</td>
+									<td>{{$contact->Mobile_Phone}}</td>
+									<td>{{$contact->Home_Phone}}</td>
+									<td>
+											<a href="{{ route('contacts.edit',$contact->id)}}" class="btn btn-primary">Edit</a>
+									</td>
+									<td>
+											<form action="{{ route('contacts.destroy', $contact->id)}}" method="post">
+												@csrf
+												@method('DELETE')
+												<button class="btn btn-danger" type="submit">Delete</button>
+											</form>
+									</td>
+							</tr>
+							@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
+@endsection
