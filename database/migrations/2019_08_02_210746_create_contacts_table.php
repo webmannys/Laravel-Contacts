eee05821e540c55ae434026423b9711ba94da4a0
Manny Sangha <freelance2@inkagency.com>
2019-08-14 03:56:53 +0000

    Fixed the id on users table because I was getting foreign key error when running migrations.

--- database/migrations/2019_08_02_210746_create_contacts_table.php
+++ database/migrations/2019_08_02_210746_create_contacts_table.php
@@ -14,19 +14,20 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-						$table->string('First_Name');
-						$table->string('Last_Name');
-						$table->string('Street')->nullable()->change();
-						$table->string('City')->nullable()->change();
-						$table->string('State')->nullable()->change();
-						$table->integer('Zip')->nullable()->change();
-						$table->string('Mobile_Phone');
-						$table->string('Home_Phone')->nullable()->change();
-            $table->timestamps();
+					$table->bigIncrements('id');
+					$table->integer('user_id')->unsigned();
+					$table->foreign('user_id')->references('id')->on('users');
+					$table->string('First_Name');
+					$table->string('Last_Name');
+					$table->string('Street')->nullable()->change();
+					$table->string('City')->nullable()->change();
+					$table->string('State')->nullable()->change();
+					$table->integer('Zip')->nullable()->change();
+					$table->string('Mobile_Phone');
+					$table->string('Home_Phone')->nullable()->change();
+					$table->timestamps();
         });
+
     }
 
     /**
